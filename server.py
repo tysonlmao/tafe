afc46fac1faa6d6e8b89de9e30a6c9ee6d21c943
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-12-05 13:43:06 +1100

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import http.server
 import socketserver
 
-PORT = 8000
+PORT = 6969
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at port", PORT)
+    print("nice.")
     httpd.serve_forever()
