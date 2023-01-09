bb0bcab080a8c9a9c5d71df8b1cf76badf66cb05
Tyson <83587481+tysonlmao@users.noreply.github.com>
2023-01-09 14:13:54 +1100

    Update server.py

--- server.py
+++ server.py
@@ -8,5 +8,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at port", PORT)
-    print("nice.")
+    print(f"server is live on port {PORT}")
     httpd.serve_forever()
