5aef09f9c2a0549198551c97ee0f58dbc9c0a55a
Tyson <83587481+tysonlmao@users.noreply.github.com>
2023-02-23 11:56:19 +1100

    Update colourChange.js

--- colour/assets/js/colourChange.js
+++ colour/assets/js/colourChange.js
@@ -1,8 +1,8 @@
-const box = document.querySelector(".box")
+const box = document.querySelector(".box");
 
 box.addEventListener("mousemove", function (e) {
     x = e.offsetX;
     y = e.offsetY;
 
-    box.style.backgroundColor = `rgb(${x}, ${y}, ${x-y})`
+    box.style.backgroundColor = `rgb(${x}, ${y}, ${x - y})`;
 });
