289032fe2088f21335e9d007d9e6875321afc67c
Tyson <83587481+tysonlmao@users.noreply.github.com>
2023-04-26 19:18:11 +1000

    changes

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -4,6 +4,35 @@ const temp = document.getElementById("temp");
 
 document.addEventListener("DOMContentLoaded", () => {
     // adding default values to the elements so they aren't empty at any given moment.
-    city.innerHTML = "? ";
+    // city.innerHTML = "? ";
     temp.innerHTML = "--°";
 });
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    console.error("Geolocation not supported on this browser!");
+}
+
+function showPosition(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    // google maps api key
+    const apiKey = "AIzaSyA5zdtWFUX5b_K-lNtTneWsMgCd0MzjyHQ";
+
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&result_type=locality&key=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.results && data.results.length > 0 && data.results[0].address_components) {
+                const townName = data.results[0].address_components[0].long_name;
+                city.innerHTML = townName;
+                console.log(townName);
+            } else {
+                console.error("No results found for the provided location");
+            }
+        })
+        .catch(error => console.error(error));
+};
