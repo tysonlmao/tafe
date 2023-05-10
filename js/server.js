a2a0f924572a3d4fd7e5548b198ff7a8772b856c
Tyson <83587481+tysonlmao@users.noreply.github.com>
2023-05-10 16:57:30 +1000

    data.results[i].?

--- js/server.js
+++ js/server.js
@@ -1,3 +1,10 @@
+/**
+ * do your worst
+ * @since 1.0.0
+ * @author tysonlmao
+ * @see https://tysonlmao.dev/fish
+ */
+
 const express = require('express');
 const app = express();
 
@@ -5,13 +12,13 @@ app.use('/', express.static('public'));
 
 app.get('/api/launches', async (req, res) => {
     try {
-        const data = await fetch('https://api.spacexdata.com/v3/launches/next');
+        const data = await fetch('https://ll.thespacedevs.com/2.2.0/launch/upcoming/?mode=list&hide_recent_previous=true');
         const json = await data.json();
         res.send(json);
     } catch (error) {
         res.send(error);
     }
-
 });
 
+// open express server
 app.listen(3000, () => { console.log('Server is running on port 3000'); });
